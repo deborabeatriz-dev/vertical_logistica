37334fa376d03e7266511312cc068bf658ab9e4d
Debora Beatriz <deborabeatriz@argosmonitoramento.com.br>
2025-05-27 05:03:23 -0300

    first commit

---
+++ src/main/java/com/dev/vertical_logistica/VerticalLogisticaApplication.java
@@ -0,0 +1,13 @@
+package com.dev.vertical_logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerticalLogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerticalLogisticaApplication.class, args);
+	}
+
+}
